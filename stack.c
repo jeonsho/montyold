8c8f787e45a8c88dd959afd9bcef754f8051f868
jeonsho <jeonsho1999@gmail.com>
2023-12-13 05:03:20 -0800

    test

--- stack.c
+++ stack.c
@@ -1,48 +1,53 @@
 #include "monty.h"
 
-void push(stack_t **stack, int value)
-{
-    stack_t *new_node = malloc(sizeof(stack_t));
-    if (!new_node)
-    {
+void push(stack_t **stack, unsigned int n) {
+	 stack_t *new_node;
+    /* Check if n is a valid integer */
+   /* if (!isdigit(n)) {
+        fprintf(stderr, "L%u: usage: push integer\n", n);
+        exit(EXIT_FAILURE);
+    }*/
+
+    /* Implement push logic */
+    new_node = malloc(sizeof(stack_t));
+    if (new_node == NULL) {
         fprintf(stderr, "Error: malloc failed\n");
         exit(EXIT_FAILURE);
     }
 
-    new_node->n = value;
+    new_node->n = n;
     new_node->prev = NULL;
+    new_node->next = *stack;
 
-    if (*stack)
-    {
-        new_node->next = *stack;
+    if (*stack != NULL) {
         (*stack)->prev = new_node;
-    }
-    else
-    {
-        new_node->next = NULL;
     }
 
     *stack = new_node;
 }
 
-void pall(stack_t **stack)
-{
+void pall(stack_t **stack, unsigned int line_number) {
+    /* Implement pall logic */
     stack_t *current = *stack;
+    (void)line_number;
 
-    while (current)
-    {
+    while (current != NULL) {
         printf("%d\n", current->n);
         current = current->next;
     }
 }
-void free_stack(stack_t *stack)
-{
-    stack_t *temp;
 
-    while (stack)
-    {
-        temp = stack;
-        stack = stack->next;
-        free(temp);
+void free_stack(stack_t **stack) {
+    /* Implement memory deallocation logic */
+    stack_t *current = *stack;
+    stack_t *next;
+
+    while (current != NULL) {
+        next = current->next;
+        free(current);
+        current = next;
     }
+
+    *stack = NULL;
 }
+
