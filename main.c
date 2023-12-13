f0f7ae89a1781bba0237a957ff89338971859723
jeonsho <jeonsho1999@gmail.com>
2023-12-13 09:10:50 -0800

    test

--- main.c
+++ main.c
@@ -12,26 +12,15 @@
  */
 int is_valid_integer(const char *str, unsigned int line_number)
 {
-	int converted, i;
+	int converted;
 
 	if (str == NULL)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
 	}
-for (i = 0; str[i]; i++)
-	{
-		if (str[i] == '-' && i == 0)
-			continue;
-		if (str[i] < '0' || str[i] > '9')
-		{
-			 fprintf(stderr, "L%d: usage: push integer\n", line_number);
-                exit(EXIT_FAILURE);
-		
-		}
-	}
 	converted = atoi(str);
-	if (converted == 0 && str[0] != '0')
+	if (converted == 0)
 	{
 		fprintf(stderr, "L%d: usage: push integer\n", line_number);
 		exit(EXIT_FAILURE);
